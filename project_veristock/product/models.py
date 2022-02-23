e6b2d0417abf155eaac95c9ee5514cc213c22cd4
JuanDa-code <juandax123456@gmail.com>
2022-02-22 21:07:50 -0500

    Product table creation

--- project_veristock/product/models.py
+++ project_veristock/product/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100, verbose_name='Name')
+    cost_sale = models.IntegerField(verbose_name='Cost Sale')
+    brand = models.CharField(max_length=100, verbose_name='Brand')
+    reference = models.CharField(max_length=100, verbose_name='Reference')
+    quantity = models.IntegerField(verbose_name='Quantity')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = 'Product'
