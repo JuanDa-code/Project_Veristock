c8b120e4d4ae4ee0308604ad96951c85bdb69dad
JuanDa-code <juandax123456@gmail.com>
2022-02-23 15:22:21 -0500

    Database finish and User app

--- project_veristock/user/admin.py
+++ project_veristock/user/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Person, Position, Customer, Type_Document, User, User_Position
 
-# Register your models here.
+admin.site.register(Person)
+admin.site.register(Position)
+admin.site.register(Customer)
+admin.site.register(Type_Document)
+admin.site.register(User)
+admin.site.register(User_Position)
