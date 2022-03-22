ab9e9c6ba2e45310941741dee8dfc6e2a37cc1d9
JuanDa-code <juandax123456@gmail.com>
2022-03-22 15:21:09 -0500

    Crud de cargo avanzado

--- project_veristock/user/urls.py
+++ project_veristock/user/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path()
+    # Urls Position
+    path('cargo/index/', views.position, name='cargo_index'),
+    path('cargo/crear/', views.add_position, name='crear_cargo'),
+    path('cargo/eliminar/<int:id>', views.delete_position, name='eliminar_cargo'),
+    path('cargo/editar/<int:id>', views.edit_position, name='editar_cargo'),
 ]
--- project_veristock/user/views.py
+++ project_veristock/user/views.py
@@ -1,1 +1,29 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import redirect, render
+from .models import Position
+from .forms import PositionForm
+
+# CRUD Position
+
+def position(request):
+    positions = Position.objects.all()
+    return render(request, './user/cargo/index.html', {'positions': positions})
+
+def add_position(request):
+    form = PositionForm(request.POST or None, request.FILES or None)
+    if form.is_valid():
+        form.save()
+        return redirect('cargo_index')
+    return render(request, './user/cargo/crear.html', {'form': form})
+
+def edit_position(request, id):
+    position = Position.objects.get(id = id)
+    form = PositionForm(request.POST or None, request.FILES or None, instance = position)
+    if form.is_valid() and request.POST:
+        form.save()
+        return redirect('cargo_index')
+    return render(request, './user/cargo/editar.html', {'form': form})
+
+def delete_position(id):
+    position = Position.objects.get(id = id)
+    position.delete()
+    return redirect('cargo_index')
