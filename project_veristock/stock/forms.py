9d6f5ed48fbf9e33cb4e474769b6a0dd2c113f66
JuanDa-code <juandax123456@gmail.com>
2022-03-31 10:17:02 -0500

    Implemento de Boostrap en el form de Item

--- project_veristock/stock/forms.py
+++ project_veristock/stock/forms.py
@@ -11,7 +11,50 @@ class ItemForm(forms.ModelForm):
 
     class Meta:
         model = Item
-        fields = '__all__'
+        fields = ['warranty_start', 'warranty_end', 'remarks', 'serial', 'state', 'product', 'purchase', 'cost_sale']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.fields['warranty_start'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Ingrese el inicio de la garantia',
+        })
+
+        self.fields['warranty_end'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Ingrese el final de la garantia',
+        })
+
+        self.fields['remarks'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Observaciones',
+        })
+
+        self.fields['serial'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Serial',
+        })
+
+        self.fields['state'].widget.attrs.update({
+            'class': 'form-select',
+            'selected': 'Selecione el estado',
+        })
+
+        self.fields['product'].widget.attrs.update({
+            'class': 'form-select',
+            'placeholder': 'Producto',
+        })
+
+        self.fields['purchase'].widget.attrs.update({
+            'class': 'form-select',
+            'placeholder': 'Compra',
+        })
+
+        self.fields['cost_sale'].widget.attrs.update({
+            'class': 'form-control',
+            'placeholder': 'Costo venta',
+        })
         
 class ProviderForm(forms.ModelForm):
     class Meta:
