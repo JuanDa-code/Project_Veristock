077921cf45cfbbffb41cebc9a32e503525927b3c
JuanDa-code <juandax123456@gmail.com>
2022-05-13 17:19:35 -0500

    Cambio a los tamaños de las tablas de crud's.

--- project_veristock/project_veristock/static/js/navbar.js
+++ project_veristock/project_veristock/static/js/navbar.js
@@ -1,7 +1,9 @@
 const btnToggle = document.querySelector('.toggle-btn');
+const table = document.querySelector('#table');
 
 btnToggle.addEventListener('click', () => {
     document.getElementById('sidebar').classList.toggle('active');
+    table.classList.toggle('col-10');
 });
 
 const navMenu = document.querySelector("#navMenu");
